a58abccd931765bd9628cfe2d00d15c37f1e9130
Dima <dmitriy.kozyatinskiy@gmail.com>
2014-02-04 20:08:05 +0200

    # Игнорирование конфига bundler

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '1c65aa9b7ef3b07b6eb266e9bd94eab7e9c9292b2810454af1bb88c68d0c6c3b0901d6c1794686fa300802b6ab8364189df2aa65bfda227095284eb3a02a0ac8'
